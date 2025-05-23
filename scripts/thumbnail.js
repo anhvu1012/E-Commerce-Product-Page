bde594e6fa1274329bb047dd312d9ece33771700
Anh <92798974+anhvu1012@users.noreply.github.com>
2025-05-23 14:14:36 +0200

    Add interactivity for desktop thumbnail

--- scripts/thumbnail.js
+++ scripts/thumbnail.js
@@ -1,13 +1,23 @@
 // THUMBNAIL 
+const displayedImg = document.getElementById('displayed-image');
+
 const images = ["images/image-product-1.jpg", "images/image-product-2.jpg", "images/image-product-3.jpg", "images/image-product-4.jpg"];
 const previousBtn = document.getElementById('previous-btn');
 const nextBtn = document.getElementById('next-btn');
 let currentIndex = 0;
 
+const productSmallThumbnails = Array.from(document.querySelectorAll('#thumbnail-container figure'));
+
 const showCurrentImg = (index) =>{
   displayedImg.src = images[index];
   displayedImg.alt = 'Product image ' + (index + 1);
 };
+
+const removeThumbnailActiveClass = () => {
+  productSmallThumbnails.forEach((thumbnail) => {
+    thumbnail.classList.remove('thumbnail-active');
+  });
+}
 
 export const initializeThumbnail = () => {
   previousBtn.addEventListener('click', () => {
@@ -18,5 +28,14 @@ export const initializeThumbnail = () => {
   nextBtn.addEventListener('click', () => {
     currentIndex = (currentIndex + 1 + images.length) % images.length;
     showCurrentImg(currentIndex); 
+  });
+
+  // console.log(productSmallThumbnails)
+  productSmallThumbnails.forEach((thumbnail, index) => {
+    thumbnail.addEventListener('click', () => {
+      removeThumbnailActiveClass();
+      thumbnail.classList.add('thumbnail-active');
+      showCurrentImg(index);
+    });
   });
 };
