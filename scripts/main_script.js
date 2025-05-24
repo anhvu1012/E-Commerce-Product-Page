e90e707bb22ffb2a992fa727fe7b5b4a5da84f97
Anh <92798974+anhvu1012@users.noreply.github.com>
2025-05-24 08:35:27 +0200

    Remove lightbox.js

--- scripts/main_script.js
+++ scripts/main_script.js
@@ -1,11 +1,9 @@
 import { initializeNavigation } from './navigation.js';
 import { initializeCart } from './cart.js';
 import { initializeThumbnail } from './thumbnail.js';
-// import { initializeLightBox } from './lightbox.js';
 
 const init = () => {
   initializeCart();
-  // initializeLightBox();
   initializeNavigation();
   initializeThumbnail();
 }
