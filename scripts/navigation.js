d92c40243867680e24be7ce3895434c56a702220
Anh <92798974+anhvu1012@users.noreply.github.com>
2025-05-24 11:27:49 +0200

    Fix bugs

--- scripts/navigation.js
+++ scripts/navigation.js
@@ -4,7 +4,7 @@ const closeBtn = document.getElementById('close-btn');
 
 export const initializeNavigation = () => {
   menuBtn.addEventListener('click', () => {
-    navigation.style.display = 'block';
+    navigation.style.display = 'flex';
   });
   closeBtn.addEventListener('click', () => {
     navigation.style.display = 'none';
